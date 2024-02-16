f2457c457af613e5082518f343eecde9e726f88f
MdMonirHossan <mdraju01820@gmail.com>
2024-02-16 17:40:18 +0600

    Implement characters count funtion with test cases

--- src/utils/text-analyzer.util.js
+++ src/utils/text-analyzer.util.js
@@ -1,5 +1,5 @@
 /**
- * @method countWords
+ * @function countWords
  * @param {string} text: The input text for which the number of words needs to be counted.
  * @description This method will return the number of words that have been counted for the given text.
  *  The @Boolean function is used as the filter callback, which will remove any empty string from the splitted array.
@@ -8,5 +8,14 @@
 function countWords(text) {
   return text.split(/\s+/).filter(Boolean).length;
 }
+/**
+ * @function countCharacters
+ * @param {string} text: The input text for which the number of characters need to be counted.
+ * @description This method will return the number of characters that have been counted for the given text.
+ * @returns The number of characters in the input text.
+ */
+function countCharacters(text) {
+  return text.length;
+}
 
-module.exports = { countWords };
+module.exports = { countWords, countCharacters };
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,4 +1,7 @@
-const { countWords } = require("../src/utils/text-analyzer.util");
+const {
+  countWords,
+  countCharacters,
+} = require("../src/utils/text-analyzer.util");
 
 /**
  * @Test countWords
@@ -7,7 +10,7 @@ const { countWords } = require("../src/utils/text-analyzer.util");
  * @case
  *   - counts the number of words in the text
  *   - handle empty text
- *   - handle text with only whitespace
+ *   - count words with only whitespace
  *   - count words separated by different whitespace
  */
 describe("countWords", () => {
@@ -23,7 +26,7 @@ describe("countWords", () => {
     expect(result).toBe(0);
   });
 
-  it("handle text with only whitespace", () => {
+  it("count words with only whitespace", () => {
     const text = "      ";
     const result = countWords(text);
     expect(result).toBe(0);
@@ -33,5 +36,40 @@ describe("countWords", () => {
     const text = "This\nis\nDifferent \nwhite   space sentence.";
     const result = countWords(text);
     expect(result).toBe(6);
+  });
+});
+/**
+ * @Test countWords
+ * @param {String} text - The text to count the characters from.
+ * @description This test will calculate the number of characters in the given text.
+ * @case
+ *   - counts the number of characters in the text
+ *   - handle empty text
+ *   - count character with whitespace
+ *   - count with special characters
+ */
+describe("countCharacters", () => {
+  it("counts the number of characters in the text", () => {
+    const text = "This is a test sentence to count the number of characters.";
+    const result = countCharacters(text);
+    expect(result).toBe(58);
+  });
+
+  it("handle empty characters count", () => {
+    const text = "";
+    const result = countCharacters(text);
+    expect(result).toBe(0);
+  });
+
+  it("count characters with whitespace", () => {
+    const text = "      ";
+    const result = countWords(text);
+    expect(result).toBe(0);
+  });
+
+  it("count with special characters", () => {
+    const text = "!&*#$%and*(!)";
+    const result = countCharacters(text);
+    expect(result).toBe(13);
   });
 });
